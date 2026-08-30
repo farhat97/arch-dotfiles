ad11222185f1fcbda5fa20fcac2ca621832af2b2
Farhat Samara <farhatsamara@gmail.com>
2026-08-30 17:52:34 -0500

    Slight color change

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -138,7 +138,7 @@ hl.config({
         allow_tearing = false,
         layout = "dwindle",
         col = {
-            active_border = { colors = { "rgba(33ccffee)", "rgba(00ff99ee)" }, angle = 45 },
+            active_border = { colors = { "rgba(33ccffcc)", "rgba(00ff99cc)" }, angle = 45 },
             inactive_border = "rgba(595959aa)",
         },
     },
